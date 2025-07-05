1175dd4f385eddb826ac35b96f955f13d045006c
jkdeberry <debejmac@gmail.com>
2025-07-05 14:20:06 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import WeatherSearch from "./WeatherSearch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search App</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
